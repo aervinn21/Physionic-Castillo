5b19c927c594509faa8d834634c2fd9819cc1e1c
aervinn21 <arvincastillo77@gmail.com>
2023-03-27 14:44:24 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  important: true,
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+
+      colors:{
+        primary: "#23a6f0",
+        secondary: "#252b42",
+        lightgray: "#fafafa",
+        darkgray: "#737373",
+        yellow:"#f3cd03",
+      },
+
+      fontFamily:{
+        montserrat: ['Montserrat', 'sans-serif']
+      },
+
+      fontSize:{
+        vhuge: 'clamp(2.5rem, 6vw, 4rem)',
+        huge: 'clamp(2rem, 6vw, 3rem)',
+        semihuge: 'clamp(1rem, 6vw, 2.5rem)'
+      },
+      backgroundImage:{
+        'pic': "url('/dist/img/1.webp')",
+      },
+
+    },
+
+    container: {
+          center: true,
+          padding: '1rem',
+          screens: {
+            xs:'530px',
+            sm: '600px',
+            md: '728px',
+            lg: '984px',
+            xl: '1240px',
+            '2xl': '1496px',
+          },
+        }
+
+  },
+  plugins: [],
+}
